bcfe02521f89242d0947b0403d3670d4b448ca09
badermc123 <43385932+badermc123@users.noreply.github.com>
2018-11-20 18:29:59 +0300

    Add files via upload

---
+++ Sharing-BOT.js
@@ -0,0 +1,34 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+let timer;
+
+console.log("BOT ONLINE");
+
+client.on("guildMemberAdd", member => {
+
+       if(member.guild.id === '421304014928609291') {
+           d5loa.push(member.user.id)
+      }
+
+    if(d5loa.includes(member.user.id)) return;
+
+
+    timer = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000
+  let words = [`نقدم لك دعوة لسموك الراقي:rose: لتحظي بجمال حضورك وتواجدك:heart_eyes: اي مشكلة تواجهك ولا يهمك,عندك الادارة تفضل ونحل مشكلتك:blue_heart::heart: واخيرا حياااك :wink: شرفنا يا عسل:
+
+  تكفا لا تردني:revolving_hearts::fire:
+  *
+  
+  https://discord.gg/5JbxHCn
+  plz enter 
+  ):`,`حرام جميل مثلك م يكون بسيرفرنا :heart::blue_heart: https://discord.gg/5JbxHCn`,`مِن بَاب الصِدف الجَميلة،تعَال يمّكن تكون جَيتك
+  ..
+   لنا اليُوم صِدفة حلوةُ ونتعرفّ عَليك فِيها. :heart::blue_heart: https://discord.gg/5JbxHCn`]
+  setTimeout(() =>{
+  member.createDM().then(function (channel) {
+  return channel.send(`${words[Math.floor(Math.random() * words.length)]}`) 
+}).catch(console.error)
+}, timer)
+})
+
+client.login('NDMxMTMyNjkwODI2MjY0NTc4.Dkbzlw.qLwVAccmghWxjnE2rAawP0z0CbA');
---
+++ WlcBot.js
@@ -0,0 +1,29 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+let timer;
+console.log("BOT ONLINE");
+const lol =
+[
+'**Wellcome To Stability:notes::notes:...**',
+'**Wellcome To Stability:notes::notes:...**',
+'**welcome to Stability SERVER.. :wilted_rose::black_heart:**',
+'**welcome to Stability SERVER.. :wilted_rose::black_heart:**',
+'**Bienvenue à Stability :zzz::musical_keyboard:**',
+'**Bienvenue à Stability :zzz::musical_keyboard:**',
+'**Welcome To Stability  :heart:**',
+'**Welcome To Stability  :heart:**',
+'**Welcome To Stability...:frowning: :heart: **',
+'**Welcome To Stability...:frowning: :heart: **',
+'**welcome to Stability SERVER.. :wilted_rose::black_heart:**',
+'***Wellcome To Stability:notes::notes:...**',
+'**Welcome To Stability  :heart:**',
+'**welcome to Stability SERVER.. :wilted_rose::black_heart:**'
+]
+client.on('guildMemberAdd', member => {
+const codes = member.guild.channels.get("482163085809942529");
+if(!codes) return;
+if(codes) {
+    setTimeout(() => codes.send(`${lol[Math.floor(Math.random() * lol.length)]} ${member}`), 3000)       
+}
+});
+client.login('mfa.Rmvvou2R66yFSeCkr5-YHTJlc3tMIEr_lpzRJPZS1mGM1FyuyZU0_CM4WM-vwWtT3M63x-hnVrqVdOdQEo30');
